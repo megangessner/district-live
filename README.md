1ee3b84a96f1c1f2ca78ead1235fc06dab3e742b
megangessner <megan_gessner@brown.edu>
2018-10-11 14:38:23 -0400

    Updates to README, some commenting in server.js

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # district-live
-This repository contains only the required files for a web-based version of Philip Klein's redistricting work. 
+This repository contains only the required files for a web-based version of Philip Klein's redistricting work. Currently, the site only displays pre-computed redistricting plans for each state, but we hope to run the algorithm and display the results live in the future. The current version is running at ec2-18-222-6-118.us-east-2.compute.amazonaws.com. Created by Megan Gessner. 
+
+# structure
+/mini_server ~ a node server. to run it locally, cd into this directory locally (after cloning this repo) and type "node server.js". this will launch a server listening on port 8080 and it has the same functionality as the live version. 
+    /public ~ contains public files. client-side functionality in index.js
+    server.js ~ the site is a single page, so the routing set up in here is to handle get requests from index.js. i've commented what each route accomplishes in the file itself
+/web_data ~ contains all polygonal data necessary to draw a redistricted plan for each state available, pre-processed from the full district repo to be ready for this particular server 
+    /census_polygons ~ contains polygonal data of the census blocks that line the boundaries of each district. sometimes census blocks will lie on the border of two or more districts, so it is assigned to one of them in pklein's algorithm. you will see these are JSON files containing a color (district) assignment as each key with multi-polygon representation of all the census blocks that were assigned to that district as the corresponding value. 
+    /district_polygons ~ contains polygonal data of the main "power cell" of each district. if you look at the live site, and toggle "census boundary block detail" option off, you will see straight-lined districts, without the fuzzy boundaries given by the census blocks. those are the polygons stored here. 
+    
+
--- mini_server/server.js
+++ mini_server/server.js
@@ -1,4 +1,4 @@
-//SET UP EXPRESS
+/////   SET UP EXPRESS   //////
 var express = require('express');
 var app = express();
 
@@ -25,36 +25,43 @@ var stateCodes = {
 			    "AL": "Alabama","AK": "Alaska","AZ": "Arizona","AR": "Arkansas","CA": "California","CO": "Colorado","CT": "Connecticut","DE": "Delaware","DC": "District Of Columbia","FL": "Florida","GA": "Georgia","HI": "Hawaii","ID": "Idaho","IL": "Illinois","IN": "Indiana","IA": "Iowa","KS": "Kansas","KY": "Kentucky","LA": "Louisiana","ME": "Maine","MD": "Maryland","MA": "Massachusetts","MI": "Michigan","MN": "Minnesota","MS": "Mississippi","MO": "Missouri","MT": "Montana","NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey","NM": "New Mexico","NY": "New York","NC": "North Carolina","ND": "North Dakota","OH": "Ohio","OK": "Oklahoma","OR": "Oregon","PA": "Pennsylvania","RI": "Rhode Island","SC": "South Carolina","SD": "South Dakota","TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington","WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming"
 			}
 
-//ROUTING
+/////   ROUTING   //////
+
+//main page of the site
 app.get('/', function(req, res){
 
 	res.render('index.html');
 });
 
+//There is a global variable above called stateCodes, which we send along as a response to a get request in index.js in makeStateList(). 
+//This helps form the list of states on the left side of the UI
 app.get('/states', function(req, res){
 	res.send(JSON.stringify(stateCodes));
 });
 
-//This is to get all data for drawing the map
+//This is to get all data for drawing the map. Takes in a :name parameter (the state code) and a :detailopt (whether or not we should 
+//send along the census block detail)
 app.get('/state/:name/:detailopt', function(req, res){
 
-	var statejson = fs.readFileSync('states.json');
+	var statejson = fs.readFileSync('states.json'); //contains information about each state, including state boundary and census area. take a look in the file for more info
 	var state = JSON.parse(statejson).features.find(state => (state.properties.NAME == stateCodes[req.params.name]));
 
-	var	stateGeom = state.geometry;//get the geometry/coordinates corresponding to the state in question
-	var zoom = (11.5 - Math.log10(state.properties.CENSUSAREA));
+	var	stateGeom = state.geometry; //get the geometry/coordinates corresponding to the state in question
+	var zoom = (11.5 - Math.log10(state.properties.CENSUSAREA)); //a cute little equation i experimentally came up with to calculate a reasonable zoom setting for open layers
 	console.log(zoom);
 
 	var detail_opt = req.params.detailopt;
 	console.log(detail_opt);
 	var census_blocks, poly_file;
 
+	//we're about to read in the district_polygons, i.e. power cells, pre-computed for state and stored in web_data
 	fs.readFile('../web_data/district_polygons/polygons_'+req.params.name, 'utf8', function(err, data){
 
 		if (err){
 			console.log('There does not already exist a file with the polygons for this districting of this state');
 			console.log(err);
 
+			//pass in empty data so that server doesn't crash, will just show outline of state
 			var json = {"polygons":[], "geometry":stateGeom, "blocks":[], "zoom":zoom}
 
 		}
@@ -67,10 +74,12 @@ app.get('/state/:name/:detailopt', function(req, res){
 			else {
 				census_blocks = [];
 			}
+			//string processing because arrays in python are different than arrays in js.
 			var polygons = JSON.parse(data.replace(/\(/g, '\[').replace(/\)/g, '\]'));
 			var json = {"polygons":polygons, "geometry":stateGeom, "blocks": census_blocks, "zoom":zoom};
 		}
 
+		//send it back to the functions that did a get request to this route in index.js
 		res.send(JSON.stringify(json));
 	});
 });
